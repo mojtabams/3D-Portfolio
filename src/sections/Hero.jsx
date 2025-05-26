0f5a8ea40acaf5031f1ccea24040b4f1ed6ccd79
Mojtaba Mohammadi <mojtabams2001@gmail.com>
2025-05-26 19:45:54 +0330

    new file and Hero sections added

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <section id="hero" className={"relative overflow-hidden"}>
+            <div className="absolute.top-0.left-0.z-10">
+                <img src="/images/bg.png" alt="background"/>
+            </div>
+            <div className="hero-layout">
+                {/* LEFT: HERO CONTENT*/}
+                {/* RIGHT: 3D MODEL*/}
+            </div>
+        </section>
+    )
+}
+export default Hero
