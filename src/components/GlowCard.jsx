785bf5a0ec1acbf2313280a17f15e3e71a18cd4e
Mojtaba Mohammadi <mojtabams2001@gmail.com>
2025-06-02 09:43:32 +0330

    exp section added

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useRef} from 'react'
 
-const GlowCard = ({card, children}) => {
+const GlowCard = ({card, children, index}) => {
+    const cardRefs = useRef([]);
+    const handleMouseMove = (index) => (e) => {
+        const card = cardRefs.current[index];
+        if (!card) return;
+
+        // Get the Muse Position  Relative to Card
+        const rect = card.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left - rect.width / 2;
+        const mouseY = e.clientY - rect.top - rect.height / 2;
+
+        // calc the angle between the mouse and the center of the card
+        let angle = ((Math.atan2(mouseY, mouseX) * (180 / Math.PI)) + 360) % 360;
+        card.style.setProperty('--start', angle + 60);
+    }
+
     return (
-        <div className="card card-border timeline-card rounded-xl p-10">
+        <div
+            ref={(e) => (cardRefs.current[index] = e)}
+            onMouseMove={handleMouseMove(index)}
+            className="card card-border timeline-card rounded-xl p-10">
             <div className="glow"/>
             <div className="flex items-center gap-1 mb-5">
                 {Array.from({length: 5}, (_, i) => (
